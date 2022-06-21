c9778ddc061a035b4aa9ad8838e0afba806aedae
Heston Snodgrass <hsnodgrass3@gmail.com>
2022-06-21 13:48:21 -0700

    fixed issue with undef title in apt_source

--- site-modules/profile/manifests/cem_gha_runner_venv/gcloud.pp
+++ site-modules/profile/manifests/cem_gha_runner_venv/gcloud.pp
@@ -7,6 +7,8 @@ class profile::cem_gha_runner_venv::gcloud {
       profile::cem_gha_runner_venv::apt_source { 'google-cloud-sdk.list':
         url          => 'https://packages.cloud.google.com/apt',
         distribution => 'cloud-sdk',
+        gpg_url      => 'https://packages.cloud.google.com/apt/doc/apt-key.gpg',
+        gpg_target   => 'cloud.google.gpg',
       }
       ~> package { 'google-cloud-cli':
         ensure          => 'present',
